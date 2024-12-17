f5da11b730b6e0d5e0099c83ae142201eeb9b14f
kakeru5015 <tesuto5015@gmail.com>
2024-12-17 18:00:51 +0900

    Add files via upload

---
+++ kadai_014/kadai_014/square.js
@@ -0,0 +1,17 @@
+//関数呼び出し
+const threekinds = ()=> {
+  console.log("八咫鏡");
+  console.log("草薙剣");
+  console.log("八尺瓊勾玉");
+}
+
+threekinds();
+
+//関数戻り値
+const shopping = (num) =>{
+ return num * num;
+}
+
+
+
+console.log(shopping(3));
